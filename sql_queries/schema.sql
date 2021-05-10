64a318a9b53fe7f493a0e33ffa1a95bf550823aa
swati <swati.bhatia@simplilearn.net>
2021-05-10 13:22:33 +0530

    sql queries data

---
+++ sql_queries/schema.sql
@@ -0,0 +1,24 @@
+create database if not exists `inventory-app`;
+
+use `inventory-app`;
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `created_by` varchar(45) NOT NULL,
+  `created_on` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+CREATE TABLE `products` (
+  `product_id` int(11) NOT NULL AUTO_INCREMENT,
+  `product_name` varchar(45) NOT NULL,
+  `category` varchar(45) NOT NULL,
+  `stock_count` varchar(45) NOT NULL DEFAULT '30',
+  `status` varchar(45) NOT NULL DEFAULT '1',
+  `purchased_date` datetime DEFAULT NULL,
+  PRIMARY KEY (`product_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+
